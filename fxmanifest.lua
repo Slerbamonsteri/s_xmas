a53d79b89889be1de3800290329ca21427c14b1f
Slerbamonsteri <75551384+Slerbamonsteri@users.noreply.github.com>
2022-11-24 20:54:00 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+
+games { 'gta5' }
+
+description 'Happy Holidays'
+
+version '1.0'
+
+client_scripts {
+    'cl.lua',
+    'config.lua',
+}
+
+server_script {
+     'sv.lua',
+     'config.lua',
+}
